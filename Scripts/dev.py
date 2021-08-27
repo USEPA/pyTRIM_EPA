e3cc045011a543274fc3d730c37755bd5be52a72
Cemal.Biryol@icf.com <C3vr3c1@1980>
2021-08-27 12:15:25 -0400

    Added dev.py

--- Scripts/dev.py
+++ Scripts/dev.py
@@ -13,7 +13,7 @@ def print_header(msg):
 
 
 def run_webapp():
-    print_header("Starting WebApp")
+    print_header("Starting WebApp Now")
 
     webapp_command = f"pipenv run webapp.py -p {WEBAPP_PORT}"
     if EXPOSE:
