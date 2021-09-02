136d7f7430a81662798b88b13a9dbff945775c97
Cemal.Biryol@icf.com <C3vr3c1@1980>
2021-09-02 09:53:46 -0400

    modifying relative paths in trim_fronend/__init__.py

--- Scripts/trim_frontend/__init__.py
+++ Scripts/trim_frontend/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_mail import Mail
 from flask_security import Security
 from flask_sqlalchemy import SQLAlchemy
-from custom.flask_api import FlaskApi
+from Scripts.custom.flask_api import FlaskApi
 from trim_db import Model as ModelClass
 from .config import init_config
 from .utils.admin_views import init_admin_views
