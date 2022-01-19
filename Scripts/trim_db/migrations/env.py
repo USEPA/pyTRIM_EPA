e3d884470860506549207e6866216bd738f1b12e
Cemal.Biryol@icf.com <C3vr3c1@1980>
2022-01-19 00:11:59 -0500

    Troubleshooting database connection issues

--- Scripts/trim_db/migrations/env.py
+++ Scripts/trim_db/migrations/env.py
@@ -14,9 +14,9 @@ def import_base_model():
         os.path.join(os.path.dirname(__file__), '..')
     )
     sys.path.insert(0, root)
-    from schema import Model
+    from ..schema import Model
     if 'user' not in Model.metadata.tables:
-        from utils.users_roles import implement_users_roles
+        from ..utils.users_roles import implement_users_roles
         try:
             implement_users_roles()
         except Exception as e:
