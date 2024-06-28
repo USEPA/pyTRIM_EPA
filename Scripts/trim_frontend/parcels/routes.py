874ba8b79eb887a562c53addc47cf391a69acbee
Berkowitz <55586@icf.com>
2024-06-28 09:47:00 -0400

    aquatic food web properties create custom params if missing

--- Scripts/trim_frontend/parcels/routes.py
+++ Scripts/trim_frontend/parcels/routes.py
@@ -374,8 +374,17 @@ def update_parcel(id, scenario_id):
             params = [pd for pd in parcels_data.keys() if pd not in ["id", "field", "csrf_token"]]
             # print(f"{field_name} {[c for c in p.compartments if c.name == comp_name[field_name]]}")
             this_comp = [c for c in p.compartments if c.name == comp_name[field_name]][0]
+            ParameterService.commit()
             for param in params:
-                this_comp.parameters[param].value = parcels_data[param]
+                this_par = this_comp.parameters[param]
+                if isinstance(this_par, ParameterDefinition):
+                    this_par = ParameterService.get_or_create(definition=this_par, scenario_id=p.scenario_id, 
+                                                            requirements=f'self.id == {this_comp.id}',
+                                                            unit=this_par.default_unit, 
+                                                            formula_id=this_par.default_formula_id
+                    )
+                this_par.value = parcels_data[param]
+            ParameterService.commit()
 
         if field_name in ["BenthicCarnivoreBiomass", "BenthicInvertebrateBiomass", "BenthicOmnivoreBiomass",
                           "MacrophyteBiomass", "WaterColumnCarnivoreBiomass", "WaterColumnHerbivoreBiomass",
@@ -402,21 +411,21 @@ def update_parcel(id, scenario_id):
 
             for this_comp in p.compartments:
                 if this_comp.name == comp_name[base_name]:
-                    print(f"{field_name} ---> {base_name} this comp {this_comp}")
-                    print(f'{prop} ---> {parcels_data[field_name]} current val {this_comp.parameters[prop].value}')
-                    if this_comp.parameters.get(prop).__tablename__ == "parameter_definition":
-                        this_req_comp = f'(self.id == {this_comp.id})'
-                        ParameterService.get_or_create(definition=this_comp.parameters.get(prop)[0],
-                                                       scenario=p.scenario,
-                                                       requirements=this_req_comp,
-                                                       value=float(parcels_data[field_name]),
-                                                       unit=this_comp.parameters.get(prop)[0].default_unit)
-                    else:
-                        this_comp.parameters.get(prop).value = float(parcels_data[field_name])
+                    this_param = this_comp.parameters.get(prop)
+                    if isinstance(this_param, ParameterDefinition):
+                        comp_req = f'(self.id == {this_comp.id})'
+                        this_param = ParameterService.get_or_create(definition=this_param,
+                                                                    scenario=p.scenario,
+                                                                    requirements=f'(self.id == {this_comp.id})',
+                                                                    unit=this_param.default_unit
+                        )
+                    this_param.value = float(parcels_data[field_name])
+                    ParameterService.commit()
+
         if field_name in ["pH", "rho", "AverageVerticalVelocity", "FractionSand", "OrganicCarbonContent",
                           "VolumeFraction_Liquid", "VolumeFraction_Vapor", "Porosity", "AirSoilBoundaryThickness",
-                          "Fractionofareaavailableforerosion", "FractionofAreaAvailableforRunoff",
-                          "Fractionofareaavailableforverticaldiffusion", "TotalRunoffRate"]:
+                          "FractionofAreaAvailableforErosion", "FractionofAreaAvailableforRunoff",
+                          "FractionofAreaAvailableforVerticalDiffusion", "TotalRunoffRate"]:
             this_comp = p.get_compartment(name=parcels_data["comp_name"])
             this_par = this_comp.parameters.get(parcels_data["field"])
 
@@ -425,11 +434,9 @@ def update_parcel(id, scenario_id):
                 this_par = ParameterService.get_or_create(definition=this_par, scenario_id=p.scenario_id, 
                                                         requirements=f'self.id == {this_comp.id}',
                                                         unit=this_par.default_unit, 
-                                                        formula_id=this_par.default_formula_id,
-                                                        value=float(parcels_data[field_name]))
-            else:
-                this_par.value = float(parcels_data[field_name])
-                ParameterService.update(this_par)
+                                                        formula_id=this_par.default_formula_id
+                )
+            this_par.value = float(parcels_data[field_name])
             ParameterService.commit()
         if field_name == "emission":
             src_comp = [c for c in p.compartments if c.name == parcels_data["compartment_name"]][0]
