3607fe284930a810045fd44396077c100df34262
55586_icfcorp <55586@icf.com>
2025-09-04 09:54:31 -0400

    adjusted aquatic food web default values

--- Scripts/trim_db/migrations/versions/2025_08_15_60eb40203023.py
+++ Scripts/trim_db/migrations/versions/2025_08_15_60eb40203023.py
@@ -1,6 +1,7 @@
 """
 Farm media
 AverageVerticalVelocity
+Aquatic food web
 
 Revision ID: 60eb40203023
 Revises: 151911e8b6a9
@@ -41,6 +42,50 @@ def upgrade():
         """
     )
 
+    # Aquatic food web default values   
+    op.execute( # Benthic_Carnivore
+        """
+        INSERT INTO parameter_definition (variable_name, full_name, domain_id, default_value, default_unit) 
+        VALUES ('BiomassPerArea', 'BiomassPerArea', 9, 0.001, 'kg/m^2');
+        """
+    )
+    op.execute( # Benthic_Omnivore
+        """
+        INSERT INTO parameter_definition (variable_name, full_name, domain_id, default_value, default_unit) 
+        VALUES ('BiomassPerArea', 'BiomassPerArea', 11, 0.002, 'kg/m^2');
+        """
+    )
+    op.execute( # Water_Column_Carnivore
+        """
+        INSERT INTO parameter_definition (variable_name, full_name, domain_id, default_value, default_unit) 
+        VALUES ('BiomassPerArea', 'BiomassPerArea', 34, 0.0002, 'kg/m^2');
+        """
+    )
+    op.execute( # Water_Column_Herbivore
+        """
+        INSERT INTO parameter_definition (variable_name, full_name, domain_id, default_value, default_unit) 
+        VALUES ('BiomassPerArea', 'BiomassPerArea', 35, 0.002, 'kg/m^2');
+        """
+    )
+    op.execute( # Water_Column_Omnivore
+        """
+        INSERT INTO parameter_definition (variable_name, full_name, domain_id, default_value, default_unit) 
+        VALUES ('BiomassPerArea', 'BiomassPerArea', 36, 0.0005, 'kg/m^2');
+        """
+    )
+    op.execute( # Zooplankton
+        """
+        INSERT INTO parameter_definition (variable_name, full_name, domain_id, default_value, default_unit) 
+        VALUES ('BiomassPerArea', 'BiomassPerArea', 37, 0.0064, 'kg/m^2');
+        """
+    )
+
+    op.execute( # Macrophyte
+        "UPDATE parameter_definition SET default_value = 0.5 WHERE domain_id = 23 AND variable_name = 'BiomassPerArea';"
+    )
+    op.execute( # Benthic_Omnivore
+        "UPDATE parameter_definition SET default_value = 0.25 WHERE domain_id = 11 AND variable_name = 'BW';"
+    )
 
 
 def downgrade():
@@ -56,8 +101,15 @@ def downgrade():
         "DELETE FROM formula WHERE equation = '0.2 * environment.Rain' AND description= 'default AverageVerticalVelocity';"
     )
 
-    #op.execute(
-    #    """
-    #    DELETE FROM parameter_definition WHERE variable_name = 'mixingHeight' AND domain_id = 1;
-    #    """
-    #)
+    op.execute(
+        """
+            DELETE FROM parameter_definition WHERE variable_name = 'BiomassPerArea' 
+            AND domain_id IN (9, 11, 34, 35, 36, 37);
+        """
+    )
+    op.execute( # Macrophyte
+        "UPDATE parameter_definition SET default_value = 0.6 WHERE domain_id = 23 AND variable_name = 'BiomassPerArea';"
+    )
+    op.execute( # Benthic_Omnivore
+        "UPDATE parameter_definition SET default_value = 2 WHERE domain_id = 11 AND variable_name = 'BW';"
+    )
