738049e339c17338bb7c4c1432fc26ff1d3d139e
55586_icfcorp <55586@icf.com>
2025-09-12 06:43:09 -0400

    meteo file upload column names fixes

--- Scripts/trim_frontend/scenarios/utils.py
+++ Scripts/trim_frontend/scenarios/utils.py
@@ -415,7 +415,7 @@ def meteo_wgt_avg_value_from_timeseries(par_dat, param_type):
     if param_type == "MET":
         df_met = df_met.loc[(df_met.Hour < 25)]  # drop faulty
         metcol_dict = {'Rain': (0, 1), 'AirTemperature': (200, 373), 'HorizontalWindSpeed': (0, 100),
-                       'WindDirection': (-360, 360), 'mixingHeight': (0, 10000), 'isDay': (0, 1),
+                       'WindDirection': (-360, 360), 'MixingHeight': (0, 10000), 'IsDay': (0, 1),
                        'CumulativeRain': (0, 1.6)}  # k, v represent name and min-max
         for k, v in metcol_dict.items():
             if k in df_met.columns:
