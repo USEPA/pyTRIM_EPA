3956fa7ee29a377732fcc7f9d4285d0a0f8e3fb0
63333_icfcorp <63333@icf.com>
2025-09-18 16:03:35 -0400

    remove unuse column | remove repeat function

--- Scripts/trim_frontend/static/js/table_builder.js
+++ Scripts/trim_frontend/static/js/table_builder.js
@@ -131,14 +131,6 @@ let ErrorValidation = {
         let is_valid = parseFloat(data) <= 1;
         return ErrToolTip(ele, is_valid, "Fractional value must be no greater than 1.");
     },
-    isInputAtLeast : function (ele, min) {
-        if (!this.isPositiveValue(ele)) {
-            return false;
-        }
-        let data = $(ele).val();
-        let is_valid = parseFloat(data) >= min;
-        return ErrToolTip(ele, is_valid, "Fractional value must be at least " + min + ".");
-    },
     isInSimulationTimeSpan: function(ele, datedata){
         let has_data = datedata.length > 0;
         return ErrToolTip(ele, has_data, "Input date(s) not in simulation date range.")
