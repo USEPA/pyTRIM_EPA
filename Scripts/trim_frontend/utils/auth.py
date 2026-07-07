fbb5679edd25987e46283621f8736afd5a149d41
Josiah McCoy <50466@icf.com>
2026-07-07 09:01:42 -0400

    Initial pass at adding oauth capability

--- Scripts/trim_frontend/utils/auth.py
+++ Scripts/trim_frontend/utils/auth.py
@@ -1,6 +1,10 @@
 import os
+import requests
+import secrets
 from datetime import datetime
-from flask_security import SQLAlchemyUserDatastore
+from urllib.parse import urlencode
+from flask import request, redirect, url_for, abort, session
+from flask_security import SQLAlchemyUserDatastore, current_user, login_user
 from flask_security.utils import hash_password
 
 
@@ -25,6 +29,119 @@ def define_superusers(app, security):
     security.datastore.commit()
 
 
+class FlaskOauth:
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        if hasattr(app, '_flask_oauth'):
+            raise AssertionError('Cannot register FlaskOauth twice!')
+        app._flask_oauth = self
+
+        self._app = app
+
+        self._oauth1_providers = {}
+        self.register_providers(app.config.get('OAUTH_PROVIDERS'), oauth2=False)
+
+        self._oauth2_providers = {}
+        self.register_providers(app.config.get('OAUTH2_PROVIDERS'))
+
+        @app.route('/oauth2/login/<provider_name>')
+        def oauth2_login(provider_name):
+            if not current_user.is_anonymous:
+                return redirect(url_for('index'))
+
+            provider = self._oauth2_providers.get(provider_name)
+            if provider is None:
+                abort(404)
+
+            session['oauth2_state'] = secrets.token_urlsafe(16)
+
+            query_args = {
+                'client_id': provider['client_id'],
+                'redirect_uri': self.get_callback_uri(provider),
+                'response_type': 'code',
+                'scope': ' '.join(provider['scopes']),
+                'state': session['oauth2_state'],
+            }
+
+            return redirect(provider['authorize_url'] + '?' + urlencode(query_args))
+
+    def register_providers(self, providers, oauth2=True):
+        if not providers:
+            return
+        if isinstance(providers, dict):
+            providers = [{'name': p_name, **p} for p_name, p in providers.items()]
+        for provider in providers:
+            self.register_provider(provider, oauth2=oauth2)
+
+    def get_callback_endpoint_name(self, provider):
+        return f'oidc_callback_{provider["name"]}'
+
+    def get_callback_uri(self, provider):
+        return url_for(self.get_callback_endpoint_name(provider), _external=True)
+
+    def register_provider(self, provider, oauth2=True):
+        if not oauth2:
+            return self._register_oauth1_provider(provider)
+        else:
+            self._register_oauth2_provider(provider)
+
+    def _register_oauth1_provider(self, provider):
+        raise NotImplementedError
+
+    def _register_oauth2_provider(self, provider):
+        self._oauth2_providers[provider['name']] = provider
+
+        redirect_endpoint = f'/oidc/callback/{provider["name"]}'
+        if 'redirect_endpoint' in provider:
+            redirect_endpoint = provider['redirect_endpoint']
+
+        @self._app.route(provider['redirect_endpoint'], endpoint=self.get_callback_endpoint_name(provider))
+        def oidc_callback():
+            if not current_user.is_anonymous:
+                return redirect(url_for('index'))
+
+            if request.args['state'] != session.get('oauth2_state'):
+                abort(401)
+
+            if 'code' not in request.args:
+                abort(401)
+
+            response = requests.post(
+                provider['token_url'],
+                data={
+                    'client_id': provider['client_id'],
+                    'client_secret': provider['client_secret'],
+                    'code': request.args['code'],
+                    'grant_type': 'authorization_code',
+                    'redirect_uri': self.get_callback_uri(provider),
+                },
+                headers={'Accept': 'application/json'}
+            )
+            if response.status_code != 200:
+                abort(401)
+            oauth2_token = response.json().get('access_token')
+            if not oauth2_token:
+                abort(401)
+
+            response = requests.get(provider['userinfo_url'], headers={
+                'Authorization': 'Bearer ' + oauth2_token,
+                'Accept': 'application/json',
+            })
+            if response.status_code != 200:
+                abort(401)
+            email = response.json()['email']
+
+            user = security.datastore.find_user(email=email)
+            if user is None:
+                abort(401)
+
+            login_user(user)
+            return redirect(url_for('index'))
+
+
 def init_auth(app, db, bcrypt, security):
     bcrypt.init_app(app)
     from ..users.models import User, Role
@@ -34,7 +151,7 @@ def init_auth(app, db, bcrypt, security):
         UserConfirmRegisterForm, UserForgotPasswordForm, \
         UserResetPasswordForm, UserChangePasswordForm
 
-    # Flask-Security fails to bind this correctly when using init_app;
+    # Flask-Security fails to bind _state correctly when using init_app;
     # if they fix it, this assignment will become unecessary --
     # but for now, we need it.
     security._state = security.init_app(
@@ -64,3 +181,7 @@ def init_auth(app, db, bcrypt, security):
             if u is not None:
                 return u
         return None
+
+    # Enable oauth login
+    oauth = FlaskOauth()
+    oauth.init_app(app)
