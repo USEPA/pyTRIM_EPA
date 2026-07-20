32ab2596ed1b03c54591e2adeba84d25a8cc0033
63333_icfcorp <63333@icf.com>
2026-07-20 11:21:29 -0400

    fix for 'undefined' showing in other parcels

--- Scripts/trim_db/services/entities.py
+++ Scripts/trim_db/services/entities.py
@@ -49,6 +49,11 @@ class ChemicalService(GenericService):
 
         target_rates = dict(cls.mercury_transformation_parameters)
 
+        def media_key(comp):
+            if comp.media.isa('Surface_Soil'):
+                return 'Surface_Soil'
+            return comp.media.name
+
         mtr_params = {}
         for comp in parcel.compartments:
             if not (
@@ -59,8 +64,9 @@ class ChemicalService(GenericService):
                 or comp.media.isa('Sediment')
             ):
                 continue
-            mtr_params[comp.media.name] = {chem_name: {} for chem_name in target_rates}
-            media_params = mtr_params[comp.media.name]
+            key = media_key(comp)
+            mtr_params[key] = {chem_name: {} for chem_name in target_rates}
+            media_params = mtr_params[key]
             for chem in mercuries:
                 for rate in target_rates[chem.name]:
                     try:
