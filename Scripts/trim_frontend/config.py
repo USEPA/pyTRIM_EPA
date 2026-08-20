a16a841af3d05805d4d97501399983075195ca2e
Max Berkowitz <55586@icf.com>
2026-08-20 08:28:06 -0400

    removed debugging

--- Scripts/trim_frontend/config.py
+++ Scripts/trim_frontend/config.py
@@ -1,4 +1,4 @@
-import os, json
+import os
 from trim_db.services.engine import get_env_db_uri
 
 
@@ -86,8 +86,6 @@ class AppConfig:
 
     TRIM_ENV_PROFILE = os.getenv('TRIM_ENV_PROFILE', 'local')
     print(f"LOADED TRIM_ENV_PROFILE: {TRIM_ENV_PROFILE}")
-
-    TEST = json.dumps(dict(os.environ))
 
 
 class ProdConfig(AppConfig):
