51abf007ac30e2515d941ec1ecb23f605bff9980
Max Berkowitz <55586@icf.com>
2026-08-30 09:33:24 -0400

    secretsmanager test

--- Scripts/trim_frontend/base/routes.py
+++ Scripts/trim_frontend/base/routes.py
@@ -7,6 +7,26 @@ base = Blueprint('base', __name__)
 
 @base.route('/', methods=['GET'])
 def index():
+    import os, json, boto3
+    def get_client_secret(secret_id):
+        # TESTING ACCESS DMAP SECRETS MANAGER
+        if not secret_id:
+            return None
+        secrets_client = boto3.client('secretsmanager')
+        response = secrets_client.get_secret_value(
+            SecretId=secret_id
+        )
+        try:
+            secret_data = json.loads(response['SecretString'])
+        except:
+            secret_data = response['SecretString']
+        return secret_data['client_secret']
+
+    try:
+        return get_client_secret(os.getenv('LOGIN_GOV_SECRET_ARN')) or "None!"
+    except Exception as e:
+        return e
+
     scenarios = []
     if current_user.is_authenticated:
         return redirect(url_for('scenario.view_scenarios'))
