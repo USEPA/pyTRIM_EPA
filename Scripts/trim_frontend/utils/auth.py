a51d1f15b0511bea21f29308722566d5685a1aee
Max Berkowitz <55586@icf.com>
2026-08-30 09:51:45 -0400

    debugging oidc

--- Scripts/trim_frontend/utils/auth.py
+++ Scripts/trim_frontend/utils/auth.py
@@ -116,6 +116,7 @@ class FlaskOauth:
 
         @self._app.route(provider['redirect_endpoint'], endpoint=self.get_callback_endpoint_name(provider))
         def oidc_callback():
+            """
             if not current_user.is_anonymous:
                 return redirect(url_for('index'))
 
@@ -156,6 +157,68 @@ class FlaskOauth:
 
             login_user(user)
             return redirect(url_for('index'))
+            """
+            datacollect={}
+            try:
+                
+                if not current_user.is_anonymous:
+                    return redirect(url_for('index'))
+
+                if request.args['state'] != session.get('oauth2_state'):
+                    raise
+
+                if 'code' not in request.args:
+                    raise
+
+
+                datacollect['authcode_POST'] = {
+                        'client_id': provider['client_id'],
+                        'client_secret': provider['client_secret'],
+                        'code': request.args['code'],
+                        'grant_type': 'authorization_code',
+                        'redirect_uri': self.get_callback_uri(provider),
+                    }
+
+                response = requests.post(
+                    provider['token_url'],
+                    data={
+                        'client_id': provider['client_id'],
+                        'client_secret': provider['client_secret'],
+                        'code': request.args['code'],
+                        'grant_type': 'authorization_code',
+                        'redirect_uri': self.get_callback_uri(provider),
+                    },
+                    headers={'Accept': 'application/json'}
+                )
+                datacollect['authcode_POST_rsp'] = response
+                
+                if response.status_code != 200:
+                    raise
+                oauth2_token = response.json().get('access_token')
+                if not oauth2_token:
+                    raise
+
+                response = requests.get(provider['userinfo_url'], headers={
+                    'Authorization': 'Bearer ' + oauth2_token,
+                    'Accept': 'application/json',
+                })
+
+                datacollect["userinfo_rsp"] = response
+
+                if response.status_code != 200:
+                    raise
+                email = response.json()['email']
+
+                user = self._security.datastore.find_user(email=email)
+                if user is None:
+                    raise
+
+                login_user(user)
+                return redirect(url_for('index'))
+            except Exception as e:
+                import traceback
+                errstr = f"{datacollect}\n\n{traceback.format_exc()}"
+                return errstr
 
 
 def init_auth(app, db, bcrypt, security):
